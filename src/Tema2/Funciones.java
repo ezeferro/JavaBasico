3f4c5ca472ebb865b7fdff575e111f75a6b0427d
Ezequiel Ferro <42ezequiel@gmail.com>
2023-05-23 16:00:15 -0300

    ejercicio tema 2

--- src/Tema2/Funciones.java
+++ src/Tema2/Funciones.java
@@ -1,9 +1,25 @@
 package Tema2;
 
-public class Funciones {
+import java.util.Scanner;
+
+/**
+ * En esta clase implementamos un metodo getPrecioIva
+ * que agrega el IVA al precio ingresado por pantalla
+ * y luego lo mostramos en la salida standard
+ */
+public class Funciones{
 
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Ingrese un precio:");
+        System.out.println("Precio + IVA = " + getPrecioIva( s.nextFloat() ) );
+
+    }
+
+    public static float getPrecioIva( float precio ){
+        return precio * 1.21f;
     }
 
 }
