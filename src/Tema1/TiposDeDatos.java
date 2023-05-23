cf6e2f07d35a757f63e2f2a1babf8abee16830db
Ezequiel Ferro <42ezequiel@gmail.com>
2023-05-23 10:16:25 -0300

    resolucion ejercicio 1

---
+++ src/Tema1/TiposDeDatos.java
@@ -0,0 +1,50 @@
+package Tema1;
+
+/**
+ * Clase para resolver primer Ejercicio
+ * del Curso de Java Basico
+ */
+
+public class TiposDeDatos {
+
+    /**
+     * Imprimo valores de diferentes tipos de datos
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        //enteros
+        byte var1 = 10;
+        System.out.println(var1);
+        int var2  = 13;
+        System.out.println(var2);
+        short var3 = 15;
+        System.out.println(var3);
+        long var4 = 12000;
+        System.out.println(var4);
+
+        //decimales
+        float var5 = 7.5f;
+        System.out.println(var5);
+        double var6 = 8.02d;
+        System.out.println(var6);
+
+        //booleanos
+        boolean var7 = false;
+        System.out.println(var7);
+        boolean var8 = true;
+        System.out.println(var8);
+
+        //Texto
+        String var9 = "hola mundo!";
+        System.out.println(var9);
+
+        char var10 = 'a';
+        System.out.println(var10);
+
+
+
+    }
+
+}
